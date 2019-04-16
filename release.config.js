74837fdd1379289a86bb1ac7e1253818277e3bbc
Matt Strom <strom.matt@gmail.com>
2019-04-16 15:49:15 -0700

    build: introduce semantic-release

---
+++ release.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	branch: 'master',
+	tagFormat: '${version}',
+	plugins: [
+		'@semantic-release/commit-analyzer',
+		'@semantic-release/release-notes-generator',
+		'@semantic-release/changelog',
+		'@semantic-release/npm',
+		['@semantic-release/git', {
+			message: 'chore(release): ${nextRelease.version}\n\n${nextRelease.notes}',
+			assets: [
+				'CHANGELOG.md',
+				'package.json',
+				'package-lock.json'
+			]
+		}]
+	]
+};
