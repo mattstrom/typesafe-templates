043c4a18d584909211f1e16cd7165eb8956964f7
Matt Strom <strom.matt@gmail.com>
2019-06-03 16:47:07 -0700

    fix: references to ts files across project boundaries

--- src/app/tags/if.ts
+++ src/app/tags/if.ts
@@ -1,9 +1,5 @@
 import { NodePath } from '@babel/traverse';
-import { JSXElement } from '@babel/types';
-
-import { evaluateJSXAttribute, getDataForScope, resolveControlFlowTag } from '../helpers';
-import { Handler } from './tags';
-
+import { getJSXElementName } from '../helpers';
 
 /**
  *  Noop function to provides type definition for JSX element.
@@ -12,23 +8,7 @@ export function $if(props: { test: any }): boolean {
 	return true;
 }
 
-export const handleIfElement: Handler = (path: NodePath<JSXElement>, state: any) => {
-	const $data = getDataForScope(path, '$data');
-	const condition = evaluateJSXAttribute(path, 'test');
-
-	const { blocks } = resolveControlFlowTag(path, state);
-
-	if (!blocks || blocks.length === 0) {
-		return;
-	}
-
-	if (condition) {
-		path.insertAfter(blocks)
-			.forEach((childPath: NodePath) => {
-				childPath.setData('$data', $data);
-				childPath.setData('$parent', $data);
-			});
-	}
-
-	path.remove();
-};
+export function isIfElement(node: NodePath) {
+	const name = getJSXElementName(node);
+	return (name === '$if');
+}
